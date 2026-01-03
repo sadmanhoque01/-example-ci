6bb7175d9529d015e584935e4e4993ea912c3f81
Sadman Hoque <sadmanhoque01@gmail.com>
2026-01-03 13:52:45 +0600

    All code

--- functions.py
+++ functions.py
@@ -9,6 +9,5 @@ def multiply(a, b):
 
 def convert_fahrenheit_to_celsius(fahrenheit):
     if fahrenheit<-459.67:
-        raise  AssertionError("Temo below abs zero")
+        raise  AssertionError("Temp below abs zero")
     return multiply(subtract(fahrenheit, 32), 5 / 9)
-
